a1d60a06ffbc2b2989e89aa0de0524e1cf82ff09
canberk-yilmaz <cyilmaz089@gmail.com>
2022-08-26 00:36:04 +0300

    link add to img for routing

--- client/components/Products.js
+++ client/components/Products.js
@@ -1,15 +1,18 @@
 import { ProductStyle } from "../styles/ProductStyle";
+import Link from "next/link";
 
 export default function Product({ product }) {
-  const { title, price, image } = product.attributes;
+  const { title, price, image, slug } = product.attributes;
 
   return (
     <ProductStyle>
       <div>
-        <img
-          src={image.data.attributes.formats.small.url}
-          alt={image.data.attributes.formats.thumbnail.name}
-        />
+        <Link href={`/product/${slug}`}>
+          <img
+            src={image.data.attributes.formats.small.url}
+            alt={image.data.attributes.formats.thumbnail.name}
+          />
+        </Link>
       </div>
       <h2>{title}</h2>
       <h3>{price} $</h3>
