c7cd01262782c1d6087780c39224bda12f159709
canberk-yilmaz <cyilmaz089@gmail.com>
2022-08-25 16:46:55 +0300

    query add to get one product, filter with slug

--- client/lib/query.js
+++ client/lib/query.js
@@ -20,3 +20,26 @@ query {
   }
   
 `;
+
+export const GET_PRODUCT_QUERY = `
+query getProduct($slug: String!) {
+    products(filters: {slug: {eq: $slug}}) {
+      data {
+        attributes {
+          title
+          description
+          price
+          slug
+          image {
+            data {
+              attributes {
+                formats
+              }
+            }
+          }
+        }
+      }
+    }
+  }
+
+`;
