e5dbd162e95e02ff17a1d7d8b7efceb3004762b7
canberk-yilmaz <cyilmaz089@gmail.com>
2022-09-06 23:10:44 +0300

    animation add for cart items

--- client/components/Cart.js
+++ client/components/Cart.js
@@ -5,11 +5,28 @@ import {
   Card,
   CardInfo,
   EmptyStyle,
-  Checkout
+  Checkout,
+  Cards,
 } from "../styles/CartStyles";
 import { Quantity } from "../styles/ProductDetails";
 import { FaShoppingCart } from "react-icons/fa";
 import { AiFillPlusCircle, AiFillMinusCircle } from "react-icons/ai";
+
+const card = {
+  hidden: { opacity: 0, scale: 0.8 },
+  show: { opacity: 1, scale: 1 },
+};
+
+const cards = {
+  hidden: { opacity: 1 },
+  show: {
+    opacity: 1,
+    transition: {
+      delayChildren: 0.4,
+      staggerChildren: 0.15,
+    },
+  },
+};
 
 export default function Cart() {
   const {
@@ -21,40 +38,57 @@ export default function Cart() {
     totalPrice,
   } = useStateContext();
   return (
-    <CartWrapper onClick={() => setShowCart(false)}>
-      <CartStyle onClick={(e) => e.stopPropagation()}>
+    <CartWrapper
+      animate={{ opacity: 1 }}
+      initial={{ opacity: 0 }}
+      transition={{ type: "tween" }}
+      exit={{ opacity: 0 }}
+      onClick={() => setShowCart(false)}
+    >
+      <CartStyle
+        initial={{ x: "50%" }}
+        animate={{ x: 0 }}
+        exit={{ x: "50%" }}
+        onClick={(e) => e.stopPropagation()}
+      >
         {cartItems.length < 1 && (
-          <EmptyStyle>
+          <EmptyStyle
+            initial={{ opacity: 0, scale: 0.8 }}
+            animate={{ opacity: 1, scale: 1 }}
+            transition={{ delay: 0.15 }}
+          >
             <h1>Cart is empty 😞</h1>
             <FaShoppingCart />
           </EmptyStyle>
         )}
-        {cartItems.length >= 1 &&
-          cartItems.map((item) => {
-            return (
-              <Card>
-                <img
-                  src={item.image.data.attributes.formats.thumbnail.url}
-                  alt={item.title}
-                />
-                <CardInfo>
-                  <h3>{item.title}</h3>
-                  <h3>{item.price} $</h3>
-                  <Quantity>
-                    <button>
-                      <AiFillMinusCircle
-                        onClick={() => removeFromCart(item, 1)}
-                      />
-                    </button>
-                    <span>{item.quantity}</span>
-                    <button>
-                      <AiFillPlusCircle onClick={() => addToCart(item, 1)} />
-                    </button>
-                  </Quantity>
-                </CardInfo>
-              </Card>
-            );
-          })}
+        <Cards variants={cards} initial="hidden" animate="show">
+          {cartItems.length >= 1 &&
+            cartItems.map((item) => {
+              return (
+                <Card variants={card} key={item.slug}>
+                  <img
+                    src={item.image.data.attributes.formats.thumbnail.url}
+                    alt={item.title}
+                  />
+                  <CardInfo>
+                    <h3>{item.title}</h3>
+                    <h3>{item.price} $</h3>
+                    <Quantity>
+                      <button>
+                        <AiFillMinusCircle
+                          onClick={() => removeFromCart(item, 1)}
+                        />
+                      </button>
+                      <span>{item.quantity}</span>
+                      <button>
+                        <AiFillPlusCircle onClick={() => addToCart(item, 1)} />
+                      </button>
+                    </Quantity>
+                  </CardInfo>
+                </Card>
+              );
+            })}
+        </Cards>
         {cartItems.length >= 1 && (
           <Checkout>
             <h3>Subtotal: {totalPrice} $</h3>
